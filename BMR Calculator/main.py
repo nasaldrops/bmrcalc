c3e34a31c7f19f5414c6ebb65fe937fc6d0a0a43
nasaldrops <hellokevinoconnor@gmail.com>
2022-06-06 19:50:13 +0100

    minor edits

--- BMR Calculator/main.py
+++ BMR Calculator/main.py
@@ -33,7 +33,7 @@ def bmrcalc():
             bmr = round(88.362 + (13.397 * weight) + (4.799 * height) - (5.677 * age))
             bmrstr = 'Your BMR is: ' + str(bmr)
             print(genderselected)
-            
+                 
         else:
             bmr = round(447.593 + (9.247 * weight) + (3.098 * height) - (4.330 * age))
             bmrstr = 'Your BMR is: ' + str(bmr)
